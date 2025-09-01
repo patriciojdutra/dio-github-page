0ff82fbfad49f7f0185f7e423bb415b2ece0e671
Patricio Dutra <66104719+patriciojdutra@users.noreply.github.com>
2025-08-31 22:04:13 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = '../data/profile.json';
+    const url = 'https://raw.githubusercontent.com/patriciojdutra/dio-github-page/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
